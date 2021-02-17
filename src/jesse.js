9f67bdc35266b55215a7f86ad1211b1ef9e4acea
metzgerbatwit <metzgerb@wit.edu>
2021-02-16 23:18:58 -0500

    Add some comments to jesse.js
    Co-Authored-By: gitrog <37446909+gitrous@users.noreply.github.com>

--- src/jesse.js
+++ src/jesse.js
@@ -3,7 +3,9 @@ const PHRASES = require("./jesse.json");
 
 //Replace variables within a phrase
 function handlePhrase(message,phrase) {
-    let nickname = message.guild.members.cache.get(message.author.id).nickname;
+    let nickname = message.guild.members.cache.get(message.author.id).nickname; //To get the nickname, you have to get the member object from the guild rather than the user object from the message.
+    
+    //Make this more efficient later, especially if more conditions are added.
     phrase = phrase.replaceAll("${USER}",nickname);
     phrase = phrase.replaceAll("${USERCAPS}",nickname.toUpperCase());
     return phrase;
